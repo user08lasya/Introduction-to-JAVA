21dc6e445a8fe9bf79c630ff6e66f5c8245d0c67
user08lasya <lasyapriya0807@gmail.com>
2025-09-22 18:54:44 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+     
+        int x = 80;
+        int y = 90;
+        int z = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is: " + z);
+    }
+}
